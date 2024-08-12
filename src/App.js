20c65d58b106b21f22441e56fce950bb04f089db
sujal-kumar32 <sujalstark12345@gmail.com>
2024-08-12 19:11:37 +0530

    add new files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>
+        Score: <span id="score">0</span>
+      </h2>
+      <div id="grid"></div>
+    </>
+  );
+}
+
+export default App;
